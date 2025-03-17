c79c76a9f9a9c43274038890097c3fae0f910ca6
Ojasvi Shah <163879237+ojasvi-shah@users.noreply.github.com>
2025-03-17 13:40:58 +0530

    Update testbench.sv

--- task6/testbench.sv
+++ task6/testbench.sv
@@ -1,48 +1,59 @@
-`timescale lus/1ns
-module SB_HFOSC #(parameter integer CLKHF_DIV=1)  (
-    input CLKHFPU,
-    input CLKHFEN,
-    output CLKHF,
+`timescale 1us/1ns
+module SB_HFOSC #(parameter integer CLKHF_DIV=1)    (
+    input CLKHFPU
+    input CLKHFEN
+    output CLKHF
 );
+endmodule
+
+module SB_FGBA_DRV #(parameter integer RGB0_CURRENT=1, parameter integer RGB1_CURRENT=1, parameter integer RGB2_CURRENT=1)    (
+    input RGBLEDEN,
+    input RGB0PWM,
+    input RGB1PWM,
+    input RGB2PWM,
+    input CURREN,
+    output RGB0,
+    output RGB1,
+    output RGB2
+);
+    assign RGB0 = RGB0PWM;
+    assign RGB1 = RGB1PWM;
+    assign RGB2 = RGB2PWM;
 endmodule
 
 `include "clk_d.v"
 module testbench();
 
-//Testbench variable
-parameter HALF_PERIOD_VLK2 = 0.5;
+//Testbench variables
+parameter HALF_PERIOD_CLK2 = 0.5;
 parameter HALF_PERIOD_CLK4 = 0.25;
 
-reg clk2;
-reg clk4 = 0;
+reg clk2l
+reg clk4 = 1'b0;
 reg clk3;
 
 initial begin
-  $dumpfile("testbench.vcd");
-  $dumpvars(0, testbench);
-end
-
-initial begin
-  clk2 = 0;
-  forever begin
-              #(HALF_PERIOD_CLK2); clk2 = -clk2
-  end
+    clk2 = 1'b0;
+    forever begin
+                #(HALF_PERIOD_CLK2); clk2 = ~clk2;
+    end
 end
 
 always begin
-              #(HALF_PERIOD_CLK4); clk4 = -clk4
+                #(HALF_PERIOD_CLK4); clk4 = ~clk4;
 end
 
 initial begin
-    clk3 = 1;
+    clk3 = 1'b1;
     forever begin
-    clk3 = 1; #(0.3);
-    clk3 = 0; #(0.7);
+    clk3 = 1'b1; #(0.3);
+    clk3 = 1'b1; #(0.7)
     end
 end
 
 initial begin
-  #40 $stop;
+ #40
+        $finish;
 end
-  
+
 endmodule
