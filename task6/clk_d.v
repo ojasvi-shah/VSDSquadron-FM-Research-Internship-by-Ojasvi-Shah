d2a7486e69ebc139d214941d1f2d0cd04193c4cc
Ojasvi Shah <163879237+ojasvi-shah@users.noreply.github.com>
2025-03-17 15:07:05 +0530

    Update clk_d.v

--- task6/clk_d.v
+++ task6/clk_d.v
@@ -7,40 +7,69 @@ module clk_d (
   // inputs
   input wire clk,
   // outputs
+  output wire led_red,
+  output wire led_blue,
+  output wire led_green,
   output wire clk2,
   output wire clk3,
   output wire clk4
 );
 
-  wire        int_osc;
+  wire         int_osc;
   reg [3:0] q = 4'b0000;
   reg [1:0] b = 2'b00;
+  reg [31:0] cclkdiv = 32'd0;
 
-//--------------------------------------------------------
-//                                                      --
-//             Internal Oscilliator                     --
-//                                                      --
-//--------------------------------------------------------
-  SB_HFOSC #(.CLKHF_DIV ("0b10")) u_SB_HFOSC ( .CLKHFPU(1'b1), .CLKFEN(1'b1), .CKLFD(int_osc));
+//------------------------------------------------------
+//                                                    --
+//                  Internal Oscilliator              --
+//                                                    --
+//------------------------------------------------------
+  SB_HFOSC #(.CLFHF_DIV ("0b10")) u_SB_HFOSC ( .CLKHFPU(1b'1), .CLKHFEN(1'b1), .CLKHF(int_osc));
 
-//--------------------------------------------------------
-//                                                      --
-//              Clock Divider                           --
-//                                                      --
-//--------------------------------------------------------
-  always @(posedge clk) begin
-    q = q +1'b1
+
+//------------------------------------------------------
+//                                                    --
+//                 Clock Divider                      --
+//                                                    --
+//------------------------------------------------------
+  always @(posedge clk)
+  clkdiv = clkdiv + 1;
+  always @(posedge clkdiv[24])
+  begin
+    q = q + 1'b1;
   end
 
-assign clk2 = q[0];
-assign clk4 = q[1];
+  assign clk2 = q[0];
+  assign clk4 = q[1];
 
-always @(posedge clk) begin
-  b = b +1'b1;
-  if(b==2'b11)
-    b=0;
-end
+  always @(posedge clkdiv[24])
+  begin 
+    b = b + 1'b1;
+    if(b==2'b11)
+      b = 0;
 
-assign clk3 = b[1];
+  end
+
+  assign clk3 = b[1];
+
+  //---------------------------------------------------
+  //                                                 --
+  //         Instantiate RGB primitive               --
+  //                                                 --
+  //---------------------------------------------------
+  SB_RGBA_DRV RGB_DRIVER (
+    .RGBLEDEN(1'b1                   ),
+    .RGB0PWM (clk2),
+    .RGB1PWM (clk3),
+    .RGB2PWM (clk4),
+    .CURREN  (1'b1                   ),
+    .RGB0    (led_red                ),
+    .RGB1    (led_green              ),
+    .RGB2    (led_blue               )
+  );
+  defparam RGB_DRIVER.RGB0_CURRENT = "0b000001";
+  defparam RGB_DRIVER.RGB1_CURRENT = "0b000001";
+  defparam RGB_DRIVER.RGB2_CURRENT = "0b000001";
 endmodule
-    
+
